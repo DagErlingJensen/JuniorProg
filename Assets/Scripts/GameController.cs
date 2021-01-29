81c043b4cb4ada1a0e80404ba1be69d3a6bab268
Dag-Erling <dagerling.jensen@kristiania.no>
2021-01-29 13:42:22 +0100

    Skriv her
    Første commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+ [Header("Coin Settings")]
+    public GameObject coinPickUp;
+    
+
+ [Header ("Bomb Settings")]
+    public GameObject bombPickUp;
+    
+
+    [Header("Spawn Area")]
+    public float maxYSpawn;
+    public float minYSpawn;
+    public float distanceFromPlayerX;
+    
+    [Header("Spawn Rate")]
+    public float startDelay;
+    public float repeatRate;
+    
+
+    private int randomSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRoll", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnRoll()
+    {
+        randomSpawn = Random.Range(1, 10);
+
+        if (randomSpawn < 5)
+        {
+            Debug.Log("Spawn Coin");
+        }
+        else {
+            Debug.Log("Spawn Bomb");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using Bolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float upForce;
+    public float gravityModifier;
+
+    public float maxHeight;
+    public float minHeight;
+
+    private Rigidbody rbPlayer;
+
+    
+    
+    void Start()
+    {
+        rbPlayer = gameObject.GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
+    }
+
+        void Update()
+    {
+         if (Input.GetKey(KeyCode.Space))
+            {
+                rbPlayer.AddForce(Vector3.up * upForce, ForceMode.Force);
+            }
+    }
+ }
+
